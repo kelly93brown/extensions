35e9f0c5ab763239180b5b6f6766afe14ff10cb7
kelly93brown <84150230+kelly93brown@users.noreply.github.com>
2025-09-09 19:16:17 +0100

    Update FaselHDPlugin.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDPlugin.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDPlugin.kt
@@ -3,7 +3,7 @@
 package com.faselhd
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin // تم الإبقاء على الصحيح فقط
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import android.content.Context
 
 @CloudstreamPlugin

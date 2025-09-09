b0f4bdeaf5f20c2ffa64c75855832ea19fe7fabc
kelly93brown <84150230+kelly93brown@users.noreply.github.com>
2025-09-09 19:07:29 +0100

    Update FaselHDPlugin.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDPlugin.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDPlugin.kt
@@ -1,15 +1,14 @@
+// المسار: FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDPlugin.kt
 
 package com.faselhd
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin // ✨ تم إضافة هذا السطر المهم
+import com.lagradost.cloudstream3.plugins.BasePlugin // تم الإبقاء على الصحيح فقط
 import android.content.Context
-import com.lagradost.cloudstream3.plugins.BasePlugin // ✨ تم التغيير إلى BasePlugin وهو الأفضل
 
 @CloudstreamPlugin
-class FaselHDPlugin: BasePlugin() { // ✨ تم التغيير إلى BasePlugin
+class FaselHDPlugin: BasePlugin() {
     override fun load(context: Context) {
-        // This function is called when the plugin is loaded
         registerMainAPI(FaselHD())
     }
 }

2f67e302a4055770a5864aceddac05894dbdc03f
kelly93brown <84150230+kelly93brown@users.noreply.github.com>
2025-09-09 21:23:47 +0100

    Delete FaselHDProvider directory

--- FaselHDProvider/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "Provides content from FaselHD"
-    authors = listOf("YourName") // Change to your name
-    status = 1 // Ok
-    tvTypes = listOf("Movie", "TvSeries", "Anime")
-}

40875f489b0beb6761055275fa25f5fb9d1dc22b
kelly93brown <84150230+kelly93brown@users.noreply.github.com>
2025-09-09 20:27:15 +0100

    Create build.gradle.kts

---
+++ FaselHDProvider/build.gradle.kts
@@ -0,0 +1,8 @@
+version = 1
+
+cloudstream {
+    description = "Provides content from FaselHD"
+    authors = listOf("YourName") // Change to your name
+    status = 1 // Ok
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+}
